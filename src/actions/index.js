8c4c796972a3683406f2c13ad46a197033fc32f8
alexey432 <alexorl432@gmail.com>
2021-04-05 14:02:43 +0300

    finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    
+    // Option 1
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // Option 2 (with _.chain from LODASH library)
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value(); //.value() = execute
+
+};
+
 export const fetchPosts = () => {
     return async (dispatch) => {
         const response = await jsonPlaceholder.get('/posts');
@@ -9,16 +25,24 @@ export const fetchPosts = () => {
     };
 };
 
-// **************WE NEED IT NOT TO MAKE MILLIONS OF GET REQUESTS (it's lodash librabry)************************
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+// ************************************************************************************************************
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({type: 'FETCH_USER', payload: response.data})
-});
+};
+// ************************************************************************************************************
+
+// **************WE NEED IT NOT TO MAKE MILLIONS OF GET REQUESTS (it's LODASH librabry)************************
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({type: 'FETCH_USER', payload: response.data})
+// });
 // ************************************************************************************************************
 
 
--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
+
 
 class UserHeader extends Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId)
-    }
-    
     render() {
         // const user = this.props.users.find((user) => user.id === this.props.userId);
         const { user } = this.props;
@@ -27,4 +23,4 @@ const mapStateToProps = (state, ownProps) => {
     return {user: state.users.find(user => user.id === ownProps.userId)}
 }
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
